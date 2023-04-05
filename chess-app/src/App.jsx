34a95edc4d6a816dbacb68077124fb18a9498b21
Levi <you@example.com>
2023-04-05 11:54:23 +0200

    zarodolgozat

--- chess-app/src/App.jsx
+++ chess-app/src/App.jsx
@@ -1,40 +1,39 @@
-import React, { useEffect, useState } from 'react'
+
+
+import React from 'react'
 import './App.css'
-import Board from './components/Board'
-import { gameSubject, initGame, resetGame } from './components/Game'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
-function App() {
-  const [board, setBoard] = useState([])
-  const [isGameOver, setIsGameOver] = useState()
-  const [result, setResult] = useState()
-  const [turn, setTurn] = useState()
-  useEffect(() => {
-    initGame()
-    const subscribe = gameSubject.subscribe((game) => {
-      setBoard(game.board)
-      setIsGameOver(game.isGameOver)
-      setResult(game.result)
-      setTurn(game.turn)
-    })
-    return () => subscribe.unsubscribe()
-  }, [])
-  return (
-    <div className="container">
-      {isGameOver && (
-        <h2 className="vertical-text">
-          GAME OVER
-          <button onClick={resetGame}>
-            <span className="vertical-text"> NEW GAME</span>
-          </button>
-        </h2>
-      )}
-      <div className="board-container">
-        <Board  board={board} turn={turn} />
-      </div>
-      {result && <p className="vertical-text">{result}</p>}
-    </div>
-  )
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from './components/firebase'
+import Home from './components/Home'
+import UserForm from './components/UseForm'
+import GameApp from './components/GameApp'
+
+export default function App() {
+    const [user, loading, error] = useAuthState(auth)
+
+    if (loading) {
+        return 'loading ...'
+    }
+    if (error) {
+        return 'There was an error'
+    }
+    if (!user) {
+        return <UserForm/>
+    }
+
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home></Home>
+                </Route>
+                <Route path="/game/:id">
+                    <GameApp/>
+                </Route>
+            </Switch>
+        </Router>
+    )
 }
-
-export default App
